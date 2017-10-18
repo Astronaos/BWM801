3b319bc7793da50fb2455eba4bccdafc2d02cbe9
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2017-10-18 13:30:24 -0500

    add package dependencies

--- src/Xwin_main.cpp
+++ src/Xwin_main.cpp
@@ -11,7 +11,23 @@
 #include <core.hpp>
 #include <FTGL/ftgl.h>
 #include <iostream>
-#include <dirent.h> // for working with the file tree / directories
+#include <sys/types.h>
+#ifdef HAVE_DIRENT_H
+#include <dirent.h>
+#define NAMLEN(dirent) strlen ((dirent)->d_name)
+#else
+#define dirent direct
+#define NAMLEN(dirent) ((dirent)->d_namlen)
+#ifdef HAVE_SYS_NDIR_H
+#include <sys/ndir.h>
+#endif
+#ifdef HAVE_SYS_DIR_H
+#include <sys/dir.h>
+#endif
+#ifdef HAVE_NDIR_H
+#include <ndir.h>
+#endif
+#endif
 #include <thread>
 #include <GL/glext.h>
 #include <string>
