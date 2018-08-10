841c8ba421d1788db764bb315c6264d913e4b7ca
Brian W. Mulligan <astrobit2@yahoo.com>
2018-08-10 14:09:23 -0500

    fix name of year for windows datetime

--- src/win_bwm801_date_time.cpp
+++ src/win_bwm801_date_time.cpp
@@ -8,7 +8,7 @@ void date_time::Get_Current(void)
 {
 	time_t t = time(nullptr);
 	struct tm tm = *localtime(&t);
-	m_uiYear = tm.tm_year + 1900;
+	m_iYear = tm.tm_year + 1900;
 	m_uiMonth = tm.tm_mon + 1;
 	m_uiDay_of_Month = tm.tm_mday;
 	m_uiDay_of_Week;
