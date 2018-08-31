9c00019d189f9cad170cae1c9cfd4615a2473b7e
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2018-08-31 13:09:03 -0500

    pair: convert operators to inline and declare as const

--- include/bwm801_pair.h
+++ include/bwm801_pair.h
@@ -17,110 +17,119 @@ namespace bwm801
 			m_tX = i_tX;
 			m_tY = i_tY;
 		}
-		pair operator +(const pair & i_cRHO) const
+		inline pair operator +(const pair & i_cRHO) const
 		{
 			return pair(m_tX + i_cRHO.m_tX, m_tY + i_cRHO.m_tY);
 		}
-		pair & operator +=(const pair & i_cRHO)
+		inline pair & operator +=(const pair & i_cRHO)
 		{
 			m_tX += i_cRHO.m_tX;
 			m_tY += i_cRHO.m_tY;
 			return *this;
 		}
-		pair operator -(const pair & i_cRHO) const
+		inline pair operator -(const pair & i_cRHO) const
 		{
 			return pair(m_tX - i_cRHO.m_tX, m_tY - i_cRHO.m_tY);
 		}
-		pair & operator -=(const pair & i_cRHO)
+		inline pair & operator -=(const pair & i_cRHO)
 		{
 			m_tX -= i_cRHO.m_tX;
 			m_tY -= i_cRHO.m_tY;
 			return *this;
 		}
-		bool operator < (const pair & i_cRHO) const
+		inline bool operator < (const pair & i_cRHO) const
 		{
 			return (m_tX < i_cRHO.m_tX || (m_tX == i_cRHO.m_tX && m_tY < i_cRHO.m_tY));
 		}
-		bool operator <= (const pair & i_cRHO) const
+		inline bool operator <= (const pair & i_cRHO) const
 		{
 			return (m_tX < i_cRHO.m_tX || (m_tX == i_cRHO.m_tX && m_tY <= i_cRHO.m_tY));
 		}
-		bool operator > (const pair & i_cRHO) const
+		inline bool operator > (const pair & i_cRHO) const
 		{
 			return (m_tX > i_cRHO.m_tX || (m_tX == i_cRHO.m_tX && m_tY > i_cRHO.m_tY));
 		}
-		bool operator >= (const pair & i_cRHO) const
+		inline bool operator >= (const pair & i_cRHO) const
 		{
 			return (m_tX > i_cRHO.m_tX || (m_tX == i_cRHO.m_tX && m_tY >= i_cRHO.m_tY));
 		}
-		bool operator == (const pair & i_cRHO) const
+		inline bool operator == (const pair & i_cRHO) const
 		{
 			return (m_tX == i_cRHO.m_tX && m_tY == i_cRHO.m_tY);
 		}
-		bool operator != (const pair & i_cRHO) const
+		inline bool operator != (const pair & i_cRHO) const
 		{
 			return (m_tX != i_cRHO.m_tX || m_tY != i_cRHO.m_tY);
 		}
 
-		template <typename S> pair & operator =(const pair<S> &i_cRHO)
+		template <typename S> inline pair & operator =(const pair<S> &i_cRHO)
 		{
 			m_tX = (T)i_cRHO.m_tX;
 			m_tY = (T)i_cRHO.m_tY;
 			return *this;
 		}
-		template <typename S> pair(const pair<S> &i_cRHO)
+		template <typename S> inline pair(const pair<S> &i_cRHO)
 		{
 			m_tX = (T)i_cRHO.m_tX;
 			m_tY = (T)i_cRHO.m_tY;
 		}
-		void glVertex(void) const
+
+		inline void glVertex2(void) const
+		{
+			glVertex2f((GLfloat)m_tX, (GLfloat)m_tY);
+		}
+		inline void glVertex3(void) const
 		{
 			glVertex3f((GLfloat)m_tX, (GLfloat)m_tY, 0.0);
 		}
-		void glTranslate(void) const
+		inline void glVertex(void) const
+		{
+			glVertex2();
+		}
+		inline void glTranslate(void) const
 		{
 			glTranslatef((GLfloat)m_tX, (GLfloat)m_tY, 0.0);
 		}
-		void glScale(void) const
+		inline void glScale(void) const
 		{
 			glScalef((GLfloat)m_tX, (GLfloat)m_tY, 1.0);
 		}
 
-		pair <float> Scalepair(const pair <double> &i_tScalar) const
+		inline pair <float> Scalepair(const pair <double> &i_tScalar) const
 		{
 			pair<float> tRet;
 			tRet.m_tX = m_tX * i_tScalar.m_tX;
 			tRet.m_tY = m_tY * i_tScalar.m_tY;
 			return tRet;
 		}
-		pair <float> Scalepair(const pair <float> &i_tScalar) const
+		inline pair <float> Scalepair(const pair <float> &i_tScalar) const
 		{
 			pair<float> tRet;
 			tRet.m_tX = m_tX * i_tScalar.m_tX;
 			tRet.m_tY = m_tY * i_tScalar.m_tY;
 			return tRet;
 		}
-		pair operator *(const float & i_cRHO)
+		inline pair operator *(const float & i_cRHO) const
 		{
 			pair<T> cRet;
 			cRet.m_tX *= i_cRHO;
 			cRet.m_tY *= i_cRHO;
 			return cRet;
 		};
-		pair& operator *=(const float & i_cRHO)
+		inline pair& operator *=(const float & i_cRHO)
 		{
 			m_tX *= i_cRHO;
 			m_tY *= i_cRHO;
 			return *this;
 		};
-		pair operator /(const float & i_cRHO)
+		inline pair operator /(const float & i_cRHO) const
 		{
 			pair<T> cRet;
 			cRet.m_tX /= i_cRHO;
 			cRet.m_tY /= i_cRHO;
 			return cRet;
 		};
-		pair& operator /=(const float & i_cRHO)
+		inline pair& operator /=(const float & i_cRHO)
 		{
 			m_tX /= i_cRHO;
 			m_tY /= i_cRHO;
