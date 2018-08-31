e7148acc8229869d65e527a74677edd0fb100f52
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2018-08-31 13:09:34 -0500

    quad: convert operators to inline and declare as const

--- include/bwm801_quad.h
+++ include/bwm801_quad.h
@@ -17,50 +17,51 @@ namespace bwm801
 			m_tBL = i_tBL;
 			m_tTR = i_tTR;
 		}
-		quad operator +(const pair<T> & i_cRHO) const
+		inline quad operator +(const pair<T> & i_cRHO) const
 		{
 			return quad(m_tBL + i_cRHO,m_tTR + i_cRHO);
 		}
-		quad & operator +=(const pair<T> & i_cRHO)
+		inline quad & operator +=(const pair<T> & i_cRHO)
 		{
 			m_tBL += i_cRHO;
 			m_tTR += i_cRHO;
 			return *this;
 		}
-		quad operator -(const pair<T> & i_cRHO) const
+		inline quad operator -(const pair<T> & i_cRHO) const
 		{
 			return quad(m_tBL - i_cRHO,m_tTR - i_cRHO);
 		}
-		quad & operator -=(const pair<T> & i_cRHO)
+		inline quad & operator -=(const pair<T> & i_cRHO)
 		{
 			m_tBL -= i_cRHO;
 			m_tTR -= i_cRHO;
 			return *this;
 		}
-		bool operator ==(const quad<T> & i_cRHO) const
+		inline bool operator ==(const quad<T> & i_cRHO) const
 		{
 			return (i_cRHO.m_tBL == i_cRHO.m_tBL && i_cRHO.m_tTR == i_cRHO.m_tTR);
 		}
-		bool operator !=(const quad<T> & i_cRHO) const
+		inline bool operator !=(const quad<T> & i_cRHO) const
 		{
 			return (i_cRHO.m_tBL != i_cRHO.m_tBL || i_cRHO.m_tTR != i_cRHO.m_tTR);
 		}
-		pair<T> Get_Size(void) const
+		inline pair<T> Get_Size(void) const
 		{
 			return (m_tTR - m_tBL);
 		}
 
-		template <typename S> quad & operator =(const quad<S> &i_cRHO)
+		template <typename S> inline quad & operator =(const quad<S> &i_cRHO) 
+		{
+			m_tBL = (T) i_cRHO.m_tBL;
+			m_tTR = (T) i_cRHO.m_tTR;
+			return *this;
+		}
+		template <typename S> inline quad (const quad<S> &i_cRHO)
 		{
 			m_tBL = (T) i_cRHO.m_tBL;
 			m_tTR = (T) i_cRHO.m_tTR;
 		}
-		template <typename S> quad (const quad<S> &i_cRHO)
-		{
-			m_tBL = (T) i_cRHO.m_tBL;
-			m_tTR = (T) i_cRHO.m_tTR;
-		}
-		void glDrawPolygon(void)
+		inline void glDrawPolygon(void)
 		{
 			glBegin(GL_QUADS);
 				glVertex2f((GLfloat)m_tBL.m_tX,(GLfloat)m_tBL.m_tY);
@@ -69,7 +70,7 @@ namespace bwm801
 				glVertex2f((GLfloat)m_tTR.m_tX,(GLfloat)m_tBL.m_tY);
 			glEnd();
 		}
-		void glDrawOutline(void)
+		inline void glDrawOutline(void)
 		{
 			glBegin(GL_LINE_LOOP);
 				glVertex2f((GLfloat)m_tBL.m_tX,(GLfloat)m_tBL.m_tY);
@@ -77,6 +78,32 @@ namespace bwm801
 				glVertex2f((GLfloat)m_tTR.m_tX,(GLfloat)m_tTR.m_tY);
 				glVertex2f((GLfloat)m_tTR.m_tX,(GLfloat)m_tBL.m_tY);
 			glEnd();
+		}
+		inline void Initialize_Bounding_Box_Coordinate(void)
+		{
+			m_tBL.m_tX = m_tTR.m_tX = m_tBL.m_tY = m_tTR.m_tY = 0;
+		}
+		inline void Process_Bounding_Box_Coordinate(const bwm801::pair<float> &i_fCoordinate)
+		{
+			if (m_tBL.m_tX > i_fCoordinate.m_tX)
+				m_tBL.m_tX = (T)i_fCoordinate.m_tX;
+			if (m_tTR.m_tX < i_fCoordinate.m_tX)
+				m_tTR.m_tX = (T)i_fCoordinate.m_tX;
+			if (m_tBL.m_tY > i_fCoordinate.m_tY)
+				m_tBL.m_tY = (T)i_fCoordinate.m_tY;
+			if (m_tTR.m_tY < i_fCoordinate.m_tY)
+				m_tTR.m_tY = (T)i_fCoordinate.m_tY;
+		}
+		inline void Process_Bounding_Box_Coordinate(const quad<T> & i_qCoordinates)
+		{
+			if (i_qCoordinates.m_tTR.m_tX > m_tTR.m_tX)
+				m_tTR.m_tX = (T)i_qCoordinates.m_tTR.m_tX;
+			if (i_qCoordinates.m_tBL.m_tX < m_tBL.m_tX)
+				m_tBL.m_tX = (T)i_qCoordinates.m_tBL.m_tX;
+			if (i_qCoordinates.m_tTR.m_tY > m_tTR.m_tY)
+				m_tTR.m_tY = (T)i_qCoordinates.m_tTR.m_tY;
+			if (i_qCoordinates.m_tBL.m_tY < m_tBL.m_tY)
+				m_tBL.m_tY = (T)i_qCoordinates.m_tBL.m_tY;
 		}
 	};
 	quad<float> operator *(const quad<float> & i_cLHO, const float &i_dRHO);
