499f298e02920eddcb8a5f3f60658c38ca6d29d5
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2018-09-28 09:23:43 -0500

    make functions in pair and quad inline

--- include/bwm801_quad.h
+++ include/bwm801_quad.h
@@ -17,50 +17,51 @@ namespace bwm801
 			m_tBL = i_tBL;
 			m_tTR = i_tTR;
 		}
-		quad operator +(const pair<T> & i_cRHO) const
+		inline quad operator +(const pair<T> & i_cRHO) const
 		{
 			return quad(m_tBL + i_cRHO,m_tTR + i_cRHO);
 		}
-		quad & operator +=(const pair<T> & i_cRHO)
+		inline quad & operator +=(const pair<T> & i_cRHO)
 		{
 			m_tBL += i_cRHO;
 			m_tTR += i_cRHO;
 			return *this;
 		}
-		quad operator -(const pair<T> & i_cRHO) const
+		inline quad operator -(const pair<T> & i_cRHO) const
 		{
 			return quad(m_tBL - i_cRHO,m_tTR - i_cRHO);
 		}
-		quad & operator -=(const pair<T> & i_cRHO)
+		inline quad & operator -=(const pair<T> & i_cRHO)
 		{
 			m_tBL -= i_cRHO;
 			m_tTR -= i_cRHO;
 			return *this;
 		}
-		bool operator ==(const quad<T> & i_cRHO) const
+		inline bool operator ==(const quad<T> & i_cRHO) const
 		{
 			return (i_cRHO.m_tBL == i_cRHO.m_tBL && i_cRHO.m_tTR == i_cRHO.m_tTR);
 		}
-		bool operator !=(const quad<T> & i_cRHO) const
+		inline bool operator !=(const quad<T> & i_cRHO) const
 		{
 			return (i_cRHO.m_tBL != i_cRHO.m_tBL || i_cRHO.m_tTR != i_cRHO.m_tTR);
 		}
-		pair<T> Get_Size(void) const
+		inline pair<T> Get_Size(void) const
 		{
 			return (m_tTR - m_tBL);
 		}
 
-		template <typename S> quad & operator =(const quad<S> &i_cRHO)
+		template <typename S> inline quad & operator =(const quad<S> &i_cRHO) 
+		{
+			m_tBL = (T) i_cRHO.m_tBL;
+			m_tTR = (T) i_cRHO.m_tTR;
+			return *this;
+		}
+		template <typename S> inline quad (const quad<S> &i_cRHO)
 		{
 			m_tBL = (T) i_cRHO.m_tBL;
 			m_tTR = (T) i_cRHO.m_tTR;
 		}
-		template <typename S> quad (const quad<S> &i_cRHO)
-		{
-			m_tBL = (T) i_cRHO.m_tBL;
-			m_tTR = (T) i_cRHO.m_tTR;
-		}
-		void glDrawPolygon(void)
+		inline void glDrawPolygon(void)
 		{
 			glBegin(GL_QUADS);
 				glVertex2f((GLfloat)m_tBL.m_tX,(GLfloat)m_tBL.m_tY);
@@ -69,7 +70,7 @@ namespace bwm801
 				glVertex2f((GLfloat)m_tTR.m_tX,(GLfloat)m_tBL.m_tY);
 			glEnd();
 		}
-		void glDrawOutline(void)
+		inline void glDrawOutline(void)
 		{
 			glBegin(GL_LINE_LOOP);
 				glVertex2f((GLfloat)m_tBL.m_tX,(GLfloat)m_tBL.m_tY);
